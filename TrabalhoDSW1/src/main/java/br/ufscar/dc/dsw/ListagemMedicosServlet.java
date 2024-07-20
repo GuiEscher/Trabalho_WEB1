fc1aaa790238d4a7a9c5dfd8efdcc8f566490d90
Lcasmendes <lucassm@estudante.ufscar.br>
2024-07-20 18:50:37 -0300

    Homepage buscando medicos, mas sem linkar pro login

--- TrabalhoDSW1/src/main/java/br/ufscar/dc/dsw/ListagemMedicosServlet.java
+++ TrabalhoDSW1/src/main/java/br/ufscar/dc/dsw/ListagemMedicosServlet.java
@@ -12,50 +12,42 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/listagemMedicos")
+@WebServlet(urlPatterns = "/homepage")
 public class ListagemMedicosServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
-	private static List<Medico> listaMedicos = new ArrayList<>();
-   
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		try (Connection con = PostgreeDBConfig.getConnection()) {
-            String sql = "SELECT (Nome, CRM, Especialidade) FROM MEDICO";
+    private static final long serialVersionUID = 1L;
+    
+    private static List<Medico> listaMedicos = new ArrayList<>();
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (Connection con = PostgreeDBConfig.getConnection()) {
+            // Consulta no SQL
+            String sql = "SELECT Nome, CRM, Especialidade FROM MEDICO";
             try (PreparedStatement stmt = con.prepareStatement(sql)) {
-                
-            	ResultSet rs = stmt.executeQuery();
-            	listaMedicos.clear(); 
-            	
-            	while(rs.next()) {
-            		Medico aux = new Medico();
-            		
-            		aux.setNome("Nome");
-            		aux.setCRM("CRM");
-            		aux.setEspecialidade("Especialidade");
-            		listaMedicos.add(aux);
-            		
-            		System.out.println(aux.getNome());
-            		
-            	}
+                ResultSet rs = stmt.executeQuery();
+                listaMedicos.clear(); 
+
+                while (rs.next()) {
+                    Medico aux = new Medico();
+
+                    // Usar os valores do ResultSet
+                    aux.setNome(rs.getString("Nome"));
+                    aux.setCRM(rs.getString("CRM"));
+                    aux.setEspecialidade(rs.getString("Especialidade"));
+                    listaMedicos.add(aux);
+
+                    System.out.println(aux.getNome());
+                }
                 System.out.println("Listagem bem sucedida");
-                
-                
+
+                // Encaminha a requisição para a página JSP
                 request.setAttribute("Medicos", listaMedicos);
-        		RequestDispatcher rd = request.getRequestDispatcher("/home.jsp");
-        		rd.forward(request, response);
+                RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
+                rd.forward(request, response);
             }
         } catch (SQLException e) {
+            // Tratamento da excessão
             e.printStackTrace();
             System.out.println("Erro na listagem");
         }
-		
-	}
-
-	/**
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}  */
-
+    }
 }
--- TrabalhoDSW1/src/main/java/br/ufscar/dc/dsw/Medico.java
+++ TrabalhoDSW1/src/main/java/br/ufscar/dc/dsw/Medico.java
@@ -1,42 +1,49 @@
 package br.ufscar.dc.dsw;
 
 public class Medico {
-	private String CRM;
-	private String Nome;
-	private String Email;
-	private String Senha;
-	private String Especialidade;
-	
-	
-	public String getEspecialidade() {
-		return Especialidade;
-	}
-	public void setEspecialidade(String especialidade) {
-		Especialidade = especialidade;
-	}
-	public String getSenha() {
-		return Senha;
-	}
-	public void setSenha(String senha) {
-		Senha = senha;
-	}
-	public String getEmail() {
-		return Email;
-	}
-	public void setEmail(String email) {
-		Email = email;
-	}
-	public String getNome() {
-		return Nome;
-	}
-	public void setNome(String nome) {
-		Nome = nome;
-	}
-	public String getCRM() {
-		return CRM;
-	}
-	public void setCRM(String cRM) {
-		CRM = cRM;
-	}
-
+    private String CRM;
+    private String Nome;
+    private String Email;
+    private String Senha;
+    private String Especialidade;
+    
+    // Getter e Setter para Especialidade
+    public String getEspecialidade() {
+        return Especialidade;
+    }
+    public void setEspecialidade(String especialidade) {
+        this.Especialidade = especialidade;
+    }
+    
+    // Getter e Setter para Senha
+    public String getSenha() {
+        return Senha;
+    }
+    public void setSenha(String senha) {
+        this.Senha = senha;
+    }
+    
+    // Getter e Setter para Email
+    public String getEmail() {
+        return Email;
+    }
+    public void setEmail(String email) {
+        this.Email = email;
+    }
+    
+    // Getter e Setter para Nome
+    public String getNome() {
+        return Nome;
+    }
+    public void setNome(String nome) {
+        this.Nome = nome;
+    }
+    
+    // Getter e Setter para CRM
+    public String getCRM() {
+        return CRM;
+    }
+    public void setCRM(String crm) {
+        this.CRM = crm;
+    }
 }
